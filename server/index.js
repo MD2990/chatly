72cb493bfb2d2083fb78e67dd5f35f9d4b4f9349
MD2990 <31358358+MD2990@users.noreply.github.com>
2022-02-13 18:21:56 +0400

    app

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const server = http.createServer(app);
 const PORT = process.env.PORT;
