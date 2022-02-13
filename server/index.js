0a07d6bd0371e8e0d7c8efeb169c3db93e992b84
MD2990 <31358358+MD2990@users.noreply.github.com>
2022-02-13 17:50:48 +0400

    t8

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT;
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chatly-three.vercel.app/",
+    origin: "https://suspicious-hodgkin-7f43d9.netlify.app/",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: false,
